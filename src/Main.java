39e248ec060279f4621200b99f378309cc413180
Tanner Mills <tanner.mills2000@gmail.com>
2023-10-18 16:25:36 -0600

    Update & Feature: Changed URL and added tle parsing.
    Summary: Changed the URL to query only the ISS one
    satellite from CelesTrak. Added the Orekit data files
    into the program.

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,61 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 import org.hipparchus.geometry.euclidean.threed.Vector3D;
+import org.orekit.data.DataContext;
+import org.orekit.data.DataProvidersManager;
+import org.orekit.data.DirectoryCrawler;
 import org.orekit.frames.FramesFactory;
 import org.orekit.orbits.*;
+import org.orekit.propagation.analytical.tle.TLE;
 import org.orekit.time.AbsoluteDate;
 import org.orekit.time.TimeScale;
 import org.orekit.time.TimeScalesFactory;
 import org.orekit.utils.Constants;
 import org.orekit.utils.PVCoordinates;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.*;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Configure Orekit
+        final File home       = new File(System.getProperty("user.home"));
+        final File orekitData = new File(home, "orekit-data");
+        if (!orekitData.exists()) {
+            System.err.format(Locale.US, "Failed to find %s folder%n",
+                    orekitData.getAbsolutePath());
+            System.err.format(Locale.US, "You need to download %s from %s, unzip it in %s and rename it 'orekit-data' for this tutorial to work%n",
+                    "orekit-data-master.zip", "https://gitlab.orekit.org/orekit/orekit-data/-/archive/master/orekit-data-master.zip",
+                    home.getAbsolutePath());
+            System.exit(1);
+        }
+        final DataProvidersManager manager = DataContext.getDefault().getDataProvidersManager();
+        manager.addProvider(new DirectoryCrawler(orekitData));
+
+        // Initiate connection
         URI uri = null;
+        String html;
+        String[] tle = new String[2];
+
         try {
-            uri = new URI("https://celestrak.org/NORAD/elements/gp.php?GROUP=gps-ops&FORMAT=tle");
+            uri = new URI("https://celestrak.org/NORAD/elements/gp.php?CATNR=25544");
             URL obj = uri.toURL();
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
             con.setRequestProperty("User-Agent", "Mozilla/5.0");
             int responseCode = con.getResponseCode();
             System.out.println("Response code: " + responseCode);
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
+            Scanner in = new Scanner(con.getInputStream());
             StringBuilder response = new StringBuilder();
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-            String html = response.toString();
-            System.out.println("HTML: " + html);
 
+            in.nextLine();
+            for (int i = 0; i < 2; i++) {
+                tle[i] = in.nextLine();
+           }
+
+
+            in.close();
         } catch (MalformedURLException | URISyntaxException e) {
             assert uri != null;
             System.out.printf("URL: \"%s\" is invalid.%n", uri);
@@ -42,8 +64,9 @@ public class Main {
             System.out.println(e.getMessage());
         }
 
-
-
+        if (TLE.isFormatOK(tle[0], tle[1])) {
+            TLE satellite = new TLE(tle[0], tle[1], TimeScalesFactory.getUTC());
+        }
 
 /*        Vector3D position = new Vector3D();
         Vector3D velocity = new Vector3D();
