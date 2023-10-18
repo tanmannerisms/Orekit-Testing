ea05543bfe301e34167bf3f439c8ea1653533bcf
Tanner Mills <tanner.mills2000@gmail.com>
2023-10-18 14:38:41 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import org.hipparchus.geometry.euclidean.threed.Vector3D;
+import org.orekit.frames.FramesFactory;
+import org.orekit.orbits.*;
+import org.orekit.time.AbsoluteDate;
+import org.orekit.time.TimeScale;
+import org.orekit.time.TimeScalesFactory;
+import org.orekit.utils.Constants;
+import org.orekit.utils.PVCoordinates;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+/*        Vector3D position = new Vector3D();
+        Vector3D velocity = new Vector3D();
+        AbsoluteDate initialDate = new AbsoluteDate(2023, 10, 18, 0, 0, 0, TimeScalesFactory.getUTC());
+
+        KeplerianOrbit orbit = new KeplerianOrbit(
+                new PVCoordinates(position, velocity),
+                FramesFactory.getEME2000(),
+                initialDate,
+                Constants.EIGEN5C_EARTH_MU
+        );*/
+    }
+}
